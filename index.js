42fafbdd3ff2bcd26cfbea2ce2eccbad400b3721
SaddockAime <aimegetz@gmail.com>
2024-03-01 14:16:37 +0200

    Updated calculator
    the updated calculator after solving division by zero error

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function calculator(num1, num2, operation){
-    switch(operation){
-        case '+': return num1+num2;
-        case '-': return num1-num2;
-        case '*': return num1*num2;
-        case '/':
-            if(num2 !==0){
-                return num1/num2;
-            }
-            else{
-                return 'Error';
-            }
-        default: return 'invalid'
+function calculate() {
+    try {
+        var expression = document.forms[0].display.value;
+        var num1 = parseFloat(expression.split(/[\+\-\*\/]/)[0]);
+        var operator = expression.split(/[\d\.]+/).find(e => e);
+        var num2 = parseFloat(expression.split(/[\+\-\*\/]/)[1]);
+        var result;
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if(num2 !== 0){
+                    result = num1 / num2;
+                } else {
+                    throw "Division by zero error";
+                }
+                break;
+            default:
+                throw "Invalid operator";
+        }
+        document.forms[0].display.value = result;
+    } catch (error) {
+        document.forms[0].display.value = 'Error';
     }
 }
-console.log(calculator(25,5,'/'));
